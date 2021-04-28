e83bcdf47fa162804d772b5eb1e1959e20931b97
Denis <denzohunter@bk.ru>
2021-04-29 00:12:13 +0300

    Второе активити с настройками выбора темы приложения

--- app/src/main/java/ru/geekbrains/calculator/ui/CalculatorActivity.java
+++ app/src/main/java/ru/geekbrains/calculator/ui/CalculatorActivity.java
@@ -1,24 +1,34 @@
 package ru.geekbrains.calculator.ui;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 import ru.geekbrains.calculator.R;
 import ru.geekbrains.calculator.logic.CalculatorAndroid;
 
 public class CalculatorActivity extends AppCompatActivity implements CalculatorView {
 
+    private static final int REQUEST_CODE = 10;
+    private static int THEME_CODE;
+
     private CalculatorPresenter presenter;
     private TextView resultTextView;
     private TextView bufferTextView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setTheme(THEME_CODE);
         setContentView(R.layout.activity_main);
 
         presenter = new CalculatorPresenter(this, new CalculatorAndroid());
@@ -92,12 +102,27 @@ public class CalculatorActivity extends AppCompatActivity implements CalculatorV
             presenter.onButtonEqualClick();
         });
 
+        Button btnSettings = findViewById(R.id.btn_settings);
 
+        btnSettings.setOnClickListener(v -> SettingsActivity.show(CalculatorActivity.this, REQUEST_CODE));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK)
+            if (requestCode == REQUEST_CODE) {
+                if (data != null) {
+                    THEME_CODE = data.getIntExtra(SettingsActivity.ARG_ANSWER, R.style.Theme_Calculator);
+                    recreate();
+                }
+            }
     }
 
     @Override
     public void showResult(String result) {
         resultTextView.setText(resultTextView.getText() + result);
     }
-
 }
